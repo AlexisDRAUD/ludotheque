6b52c2b558d1660a2180cb1c22b3eeaf69531ebf
alexisdraud <alexis.draud@gmail.com>
2025-03-19 16:03:39 +0100

    first commit

---
+++ src/main/java/fr/eni/ludothque/LudothqueApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.ludothque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudothqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudothqueApplication.class, args);
+    }
+
+}
